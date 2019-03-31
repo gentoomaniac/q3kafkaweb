da97a3060ff97953b470210f3cf512d71bf97481
Marco Siebecke <gentoomaniac@gmail.com>
2019-03-31 10:36:07 +0200

    websocket event receiving works

--- src/app.py
+++ src/app.py
@@ -3,11 +3,14 @@ import logging
 import os
 import uuid
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session
 from flask_bootstrap import Bootstrap
 from flask_socketio import SocketIO, send, emit
 
-logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+from kafka import KafkaConsumer
+from kafka.errors import KafkaError
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 log = logging.getLogger(__file__)
 
 app = Flask(__name__)
@@ -15,16 +18,33 @@ app.secret_key = os.getenv('APP_SECRET', str(uuid.uuid4()))
 bootstrap = Bootstrap(app)
 socketio = SocketIO(app)
 
+consumers = {}
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@socketio.on('event', namespace='/events')
+@socketio.on('get_all', namespace='/events')
 def handle_message(message):
-    log.debug(json.dumps(message))
-    emit('event', message)
+    log.info('client asking for all events: %s', session['kafka_id'])
+    for kafka_msg in consumers[session['kafka_id']]:
+        log.info('emitting event: %s', kafka_msg.value)  #TODO
+        emit('event', json.dumps(kafka_msg.value))
+
+
+@socketio.on('connect', namespace='/events')
+def on_connect_handler():
+    kafka_id = str(uuid.uuid4())
+    session['kafka_id'] = kafka_id
+    consumers[kafka_id] = KafkaConsumer(
+        'ioquake3',
+        group_id=kafka_id,
+        client_id=kafka_id,
+        value_deserializer=lambda m: json.loads(m.decode('ascii')),
+        bootstrap_servers=['localhost:9092'])
+    emit('connected', json.dumps({'session_id': kafka_id}))
 
 
 if __name__ == '__main__':
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,12 +1,18 @@
+var session_id;
+
 function setupSocketIO() {
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/events');
     socket.on('connect', function () {
-        socket.emit('event', { data: 'I\'m connected!' });
-        console.log('Websocket connected.')
+        console.log('Websocket connecting...')
     });
     socket.on('event', function (msg) {
-        console.log('Event received.')
+        console.log('Event received: ' + msg)
         eventHandler(msg)
+    });
+    socket.on('connected', function (msg) {
+        session_id = msg.session_id;
+        console.log('Websocket connected: ' + msg.session_id);
+        socket.emit('get_all', { data: 'Give me all you have' });
     });
     socket.on('disconnect', function () {
         console.log('Websocket disconnected.');
@@ -14,7 +20,13 @@ function setupSocketIO() {
 };
 
 function eventHandler(msg) {
-    console.log('Event handled: ' + JSON.stringify(event));
-    var old = $('#raw_events').val();
-    $('#raw_events').val(old + '\n' + JSON.stringify(event));
+    console.log('Event handled: ' + JSON.stringify(msg));
+    var textarea = $('#raw_events');
+    var old = textarea.val();
+    textarea.val(old + '\n' + JSON.stringify(msg));
+    textarea.scrollTop(textarea[0].scrollHeight - textarea.height());
+}
+
+function killEventToHTML(msg) {
+
 }
