2ffbb373f5eff9819e2eb1e3b2933630f772242d
Marco Siebecke <marco@siebecke.se>
2023-11-24 10:31:33 +0100

    fixed margins

--- q3web/src/components/Layout/Layout.js
+++ q3web/src/components/Layout/Layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Layout = ({children}) =>{
     return(
         <>
-        <div>
+        <div className="layout">
           {children}
         </div>
         </>
